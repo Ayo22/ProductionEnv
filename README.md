805b21ed93a9a5ce6782a58f02f853365a2c9da9
Sujan <sujan.sooriyakumar@gmail.com>
2020-01-14 14:31:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProductionEnv
+
+Developed with Unreal Engine 4
---
+++ Source/ProductionEnv/ProductionEnvGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProductionEnvGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRODUCTIONENV_API AProductionEnvGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
